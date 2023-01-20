747826238d9b100ef2d943ecc8f2d6f1bd5fd081
Artem Potlov <potlov.artem@gmail.com>
2023-01-20 22:37:02 +0300

    feat: add reducers

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,7 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { cashInReducer } from './slices/cashin/cashin.slice';
+import { vMachineReducer } from './slices/VMachine/vmachine.slice';
+import { walletReducer } from './slices/wallet/wallet.slice';
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    vMachine: vMachineReducer,
+    wallet: walletReducer,
+    cashIn: cashInReducer,
+  },
 });
 
 export type TRootState = ReturnType<typeof store.getState>;
